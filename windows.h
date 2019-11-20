f27672634a7ae7e4ef41d23818517a15adac767d
vinaghost <nguyentrungnhan87@gmail.com>
2019-11-20 12:41:06 +0700

    add windows class

---
+++ windows.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+#define FPS 60.0
+
+float const mSPF = 1000000.0 / FPS;
+
+class Windows {
+protected:
+	int width, height;
+	sf::RenderWindow renderWindow;
+	sf::Event event;
+	sf::Clock clock;
+public:
+	Windows(int width, int height);
+	virtual ~Windows();
+
+	virtual void draw() = 0;
+	virtual void run() = 0;
+};
+
+Windows::Windows(int width, int height) : width(width), height(height) {}
+
+Windows::~Windows() {}
