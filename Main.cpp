17f1b368cf7ffa38aa8c24bc4af6fd77856dd491
vinaghost <nguyentrungnhan87@gmail.com>
2019-11-29 10:38:46 +0700

    add globals header

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,9 @@
 ﻿//include Thư viện
 
-
 //include Thư viện
 
 int main() {
 	//Code test
-
-
-
 
 	//Code test
 	return 0;
---
+++ globals.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace globals {
+	const int SREEN_WIDTH = 800;
+	const int SCREEN_HEIGHT = 600;
+}
+namespace winner {
+	enum side {
+		NONE,
+		LEFT,
+		RIGHT
+	};
+};
